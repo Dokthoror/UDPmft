11fa1c9a815a29368cf0e4633e814a33b7af473a
Dokthoror <pascalguyguy@gmail.com>
2021-01-21 21:39:14 +0100

    Add interface name in config and more visibility

--- client/client.ts
+++ client/client.ts
@@ -1,8 +1,15 @@
 // Imports dependancies
 import * as dgram from 'dgram';
+import { NetworkInterfaceInfo, networkInterfaces } from 'os';
 import config from './config.json';
 import eventsHandler from './src/events/eventsHandler';
 import Event from './src/modules/Event';
+
+
+// Searches for the IPv4 addres of the specified interface in ./config.json
+const netInterface: NetworkInterfaceInfo[] | undefined = networkInterfaces()[config.INTERFACE];
+if (!netInterface) throw new Error(`The network interface ${config.INTERFACE} was not found.`);
+export const netAddr = netInterface.find((i: NetworkInterfaceInfo): boolean => i.family == 'IPv4')?.address;
 
 
 export const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
--- client/src/events/message/index.ts
+++ client/src/events/message/index.ts
@@ -8,6 +8,8 @@ import { WriteStream, createWriteStream } from 'fs';
 let writeToFile = false;
 let wStream: WriteStream;
 
+let packetNumber = 0;
+
 // Function to run when the event is triggered
 const run: (message: Buffer, remote: RemoteInfo) => void = (message: Buffer, remote: RemoteInfo) => {
 	// Three different formats of message :
@@ -15,7 +17,7 @@ const run: (message: Buffer, remote: RemoteInfo) => void = (message: Buffer, rem
 	//		Buffer
 	//		"STOP md5_hash_from_the_original_file"
 
-	console.log(`Message from ${remote.address}:${remote.port} - ${message.byteLength} bytes long`);
+	// console.log(`Message from ${remote.address}:${remote.port} - ${message.byteLength} bytes long`);
 	const data: string = message.toString().split(' ')[0];	// First part of message
 	const value: string = message.toString().split(' ')[1];	// Second part of message
 
@@ -25,18 +27,19 @@ const run: (message: Buffer, remote: RemoteInfo) => void = (message: Buffer, rem
 		writeToFile = true;
 		wStream = createWriteStream(`./${value}`, { flags: 'a' });	// Flag "append"
 		wStream.cork();	// Caches the data
-		console.log('START received');
+		console.log(`${packetNumber++}: START received`);
 		break;
 
 	case 'STOP':
 		writeToFile = false;
 		wStream.close();	// Closes the WriteStream and write the data in the target file
-		console.log('STOP received');
+		console.log(`${packetNumber++}: STOP received`);
 		break;
 
 	default:
 		if (writeToFile) {
 			wStream.write(message);	// Writes the data in the cache
+			console.log(`${packetNumber}: receives ${message.length} bytes`);
 		}
 		break;
 	}
